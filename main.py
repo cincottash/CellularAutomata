c4c03487c348fd2327cf876d48a4510be7ed883d
cincottash <cincotta@udel.edu>
2023-04-29 02:03:09 -0400

    comments and formatting

--- main.py
+++ main.py
@@ -18,6 +18,7 @@ These rules, which compare the behavior of the automaton to real life, can be co
     All other live cells die in the next generation. Similarly, all other dead cells stay dead.
 '''
 
+#make a grid over the background and display it to the screen
 def displayGrid(canvas):
     for pixelX in range(canvas.get_width()):
         for pixelY in range(canvas.get_height()):
@@ -35,39 +36,50 @@ def initializeBackground(canvas):
 def initializeCells(canvas):
     done = False
 
-    blockSize = int(canvas.get_width()/10)
+    #each cell is a square
+    cellSize = int(canvas.get_width()/10)
 
+    #list of cell objects which store the state (isAlive) as well as sizes and coordinate stuff
     cellList = []
 
-    for xStart in range(0,canvas.get_width(), blockSize):
-        for yStart in range(0, canvas.get_height(), blockSize):
-            cellList.append(Cell(xStart, yStart, blockSize, False))
+    #add all the cells to the list starting as dead cells
+    for xStart in range(0,canvas.get_width(), cellSize):
+        for yStart in range(0, canvas.get_height(), cellSize):
+            cellList.append(Cell(xStart, yStart, cellSize, False))
 
     while(not done):
         for event in pg.event.get():
             if event.type == pg.KEYDOWN and event.key == pg.K_RETURN:
                 done = True
+            #if we clicked mouse button 1...
             elif pg.mouse.get_pressed()[0]:
                 coords = pg.mouse.get_pos()
-                xStart = coords[0]//blockSize * blockSize
-                yStart = coords[1]//blockSize * blockSize
 
+                #find the starting x and y coords of the cell we clicked on
+                xStart = coords[0]//cellSize * cellSize
+                yStart = coords[1]//cellSize * cellSize
+
+                #check the above starting coords against the coords in our cell list to find the index of the corresponding cell in our cell list
+                #set the status of the cell to alive
+                #there can be only one cell in our list with those coords
                 for index, cell in enumerate(cellList):
                     if cell.xStart == xStart and cell.yStart == yStart:
                         cellList[index].isAlive = True
                         print(index)
                         break
 
-                for pixelX in range(xStart, xStart+blockSize):
-                    for pixelY in range(yStart, yStart+blockSize):
+                #make that cell black
+                for pixelX in range(xStart, xStart+cellSize):
+                    for pixelY in range(yStart, yStart+cellSize):
                         canvas.set_at((pixelX,pixelY), (0,0,0))
 
             pg.display.update()
 
     return cellList
 
+#draw cell to the screen according to it's size and state
 def displayCells(canvas, cellList):
-    print('Updating cells\n')
+    print('Displaying cells\n')
     for cell in cellList:
         if cell.isAlive:
             for pixelX in range(cell.xStart, cell.xStart+cell.size):
@@ -75,10 +87,12 @@ def displayCells(canvas, cellList):
                     canvas.set_at((pixelX, pixelY), (0, 0, 0))
     pg.display.update()
 
+#apply the rules of the game to the cells
 def updateCells(canvas, cellList):
 
-    return cellList
+    print('Updating cells\n')
 
+    return cellList
 
 def main():
     canvas, clock, fps = setup()
@@ -96,19 +110,23 @@ def main():
             elif event.type == pg.KEYDOWN and event.key == pg.K_q:
                 done=True
             else:
-                #only initialize the cells after we first drew all the background shit
+                #set all the pixels to white on the first loop
                 if firstRun:
                     print('initializing background\n')
                     initializeBackground(canvas)
                     
+                #make a grid over the background and display it to the screen
                 displayGrid(canvas)
 
                 if firstRun:
                     print('initializing Cells')
+                    #allow user to set the starting cells
                     cellList = initializeCells(canvas)
 
+                #apply the rules of the game to the cells
                 cellList = updateCells(canvas, cellList)
 
+                #draw cells to the screen according to it's size and state
                 displayCells(canvas, cellList)
 
                 clock.tick(fps)
